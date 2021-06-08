a799f6d6ce439653dd4428f13817e3fb3a60c873
RalXYZ <RalXYZ@pm.me>
2021-06-08 21:23:31 +0800

    add simple block allocate

--- allocator.h
+++ allocator.h
@@ -1,6 +1,9 @@
 #ifndef YAA_ALLOCATOR_H
 #define YAA_ALLOCATOR_H
 
+const std::size_t BYTES_PER_BLOCK = 16;  // 16 bytes
+
+
 namespace yaa {
     template <typename T>
     class allocator {
@@ -9,16 +12,42 @@ namespace yaa {
         typedef std::size_t size_type;
         typedef std::ptrdiff_t difference_type;
 
-        [[nodiscard]] T* allocate(std::size_t n) {
-
-        }
+        [[nodiscard]] T* allocate(std::size_t);
 
         void deallocate(T*, std::size_t);
     };
 
     template <typename T>
-    void allocator::deallocate(T* p, std::size_t n) {
+    T* allocator<T>::allocate(std::size_t n) {
+        /*
+         * since C++11, std::allocator<T>::allocate
+         * throws std::bad_array_new_length if
+         * std::numeric_limits<std::size_t>::max() / sizeof(T) < n.
+         */
+        if (std::numeric_limits<std::size_t>::max() / sizeof(T) < n) {
+            throw std::bad_array_new_length();
+        }
 
+        auto applied_bytes = n * sizeof(T);
+
+        // division in C/C++ rounds down the result, but we want to round up, so we perform "+1"
+        auto block_amount = applied_bytes / BYTES_PER_BLOCK + 1;
+
+        auto buffer = reinterpret_cast<T*>(::operator new(block_amount * BYTES_PER_BLOCK));
+
+        /*
+         * throws std::bad_alloc if allocation fails.
+         */
+        if (buffer == nullptr) {
+            throw std::bad_alloc();
+        }
+
+        return buffer;
+    }
+
+    template <typename T>
+    void allocator<T>::deallocate(T* p, std::size_t) {
+        ::operator delete(p);
     }
 }
 
